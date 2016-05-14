2c5f5f58469bac8e65b690c6f79051fa3bd49e2e
Max Kimambo <max@kimambo.de>
2016-05-14 22:41:27 +0200

    project setup

---
+++ server/config/environment/development.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+
+      uri: 'mongodb://ds011382.mlab.com:11382/cabeceo'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+npm install && cd /var/www && gulp serve
